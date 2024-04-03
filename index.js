eb7f45cc991504e413e69f321290c94964a47d6a
Vaishnavi Patil <109457185+Vaishh18@users.noreply.github.com>
2024-04-03 16:33:32 +0530

    new data added

--- index.js
+++ index.js
@@ -4,6 +4,43 @@ const app = express()
 
 const port = 4000
 
+const githubData ={
+  
+    "login": "Vaishh18",
+    "id": 109457185,
+    "node_id": "U_kgDOBoYvIQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/109457185?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Vaishh18",
+    "html_url": "https://github.com/Vaishh18",
+    "followers_url": "https://api.github.com/users/Vaishh18/followers",
+    "following_url": "https://api.github.com/users/Vaishh18/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Vaishh18/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Vaishh18/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Vaishh18/subscriptions",
+    "organizations_url": "https://api.github.com/users/Vaishh18/orgs",
+    "repos_url": "https://api.github.com/users/Vaishh18/repos",
+    "events_url": "https://api.github.com/users/Vaishh18/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Vaishh18/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Vaishnavi Patil",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 15,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 2,
+    "created_at": "2022-07-17T08:00:38Z",
+    "updated_at": "2024-04-03T10:31:13Z"
+  }
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +54,11 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send("<h2>Chai and backend</h2>")
+})
+
+
+app.get("/github",(req,res)=>{
+  res.json(githubData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
